33e93cbfae02db5780f06147215c1be1cf2431f1
Jonas Kalderstam <jonas@kalderstam.se>
2015-09-24 11:05:26 +0200

    Fix setup.py
    Now it actually includes the package.
    Also less quotes!

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+   Copyright 2015 Neo Technology
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 
 import os
-import sys
-
 try:
     from setuptools import setup, find_packages
     from setuptools.extension import Extension
@@ -9,22 +23,33 @@ except ImportError:
     from distutils.core import setup, find_packages
     from distutils.extension import Extension
 
+
+# Used for reading the README into long_description below.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 packages = find_packages(exclude=("test", "test.*"))
-package_metadata = {
-    "name": __package__,
-    "version": "1.0.0",
-    "description": "Python driver for Neo4j",
-    "long_description": "", 
-    "author": "Neo Technology",
-    "classifiers": [
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Topic :: Database",
-        "Topic :: Software Development",
-    ],
-    "zip_safe": False,
-}
 
-setup(**package_metadata)
-
+setup(name=__package__,
+      version="1.0.0",
+      description="Python driver for Neo4j",
+      license="Apache",
+      long_description=read("README.rst"),
+      author="Neo Technology",
+      keywords="neo4j graph database",
+      url="https://github.com/neo4j/neo4j-python-driver",
+      classifiers=[
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+          "Topic :: Database",
+          "Topic :: Software Development",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: Implementation :: Jython",
+      ],
+      packages=packages)
