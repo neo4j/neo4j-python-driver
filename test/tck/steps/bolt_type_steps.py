f96e37b97ebabfa824da40996648aad917eaa149
RagnarW <ragnar.wernersson@neotechnology.com>
2016-01-13 16:32:03 +0100

    Added first part of cypher compatabillity suite. Also added missing HEADER to test files

--- test/tck/steps/bolt_type_steps.py
+++ test/tck/steps/bolt_type_steps.py
@@ -1,5 +1,27 @@
-from behave import *
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 
+# Copyright (c) 2002-2016 "Neo Technology,"
+# Network Engine for Objects in Lund AB [http://neotechnology.com]
+#
+# This file is part of Neo4j.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from behave import *
+import json
+
+from neo4j.v1 import GraphDatabase
 from test.tck import tck_util
 
 use_step_matcher("re")
@@ -7,8 +29,7 @@ use_step_matcher("re")
 
 @given("A running database")
 def step_impl(context):
-    return None
-    # check if running
+    tck_util.send_string("RETURN 1")
 
 
 @given("a value (?P<input>.+) of type (?P<bolt_type>.+)")
@@ -122,3 +143,121 @@ def step_impl(context):
     for result in context.results.values():
         result_value = result[0].values()[0]
         assert result_value == context.expected
+
+
+@given("A driver containing a session pool of size (?P<size>\d+)")
+def step_impl(context, size):
+    context.driver = GraphDatabase.driver("bolt://localhost", max_pool_size=1)
+
+
+@when("acquiring a session from the driver")
+def step_impl(context):
+    context.session = context.driver.session()
+
+
+@step('with the session running the Cypher statement "(?P<statement>.+)"')
+def step_impl(context, statement):
+    context.cursor = context.session.run(statement)
+
+
+@step("pulling the result records")
+def step_impl(context):
+    context.cursor.consume()
+
+
+@then("acquiring a session from the driver should not be possible")
+def step_impl(context):
+    try:
+        context.session = context.driver.session()
+    except:
+        assert True
+    else:
+        assert False
+
+
+@then("acquiring a session from the driver should be possible")
+def step_impl(context):
+    _ = context.driver.session()
+    assert True
+
+
+@given("init: (?P<statement>.+);")
+def step_impl(context, statement):
+    tck_util.send_string(statement)
+
+
+@when("running: (?P<statement>.+);")
+def step_impl(context, statement):
+    context.results = {"as_string": tck_util.send_string(statement)}
+
+
+@given("an empty database")
+def step_impl(context):
+    tck_util.send_string("MATCH (n) DETACH DELETE n")
+
+
+@then("result should be a path p containing")
+def step_impl(context):
+    result = context.results["as_string"]
+    given = tck_util.result_to_set(result)
+    expected = tck_util.text_path_to_set(context.table)
+    if given != expected:
+        raise Exception("Path does not match given: %s expected: %s" % (given, expected))
+
+
+@then("result should be integer\(s\)")
+def step_impl(context):
+    result = context.results["as_string"]
+    given = tck_util.result_to_set(result)
+    expected = tck_util.text_int_to_set(context.table)
+    if given != expected:
+        raise Exception("Integers does not match given: %s expected: %s" % (given, expected))
+
+
+@then("result should be node\(s\)")
+def step_impl(context):
+    result = context.results["as_string"]
+    given = tck_util.result_to_set(result)
+    expected = tck_util.text_node_to_set(context.table)
+    if given != expected:
+        raise Exception("Nodes does not match given: %s expected: %s" % (given, expected))
+
+
+@then("result should be string\(s\)")
+def step_impl(context):
+    result = context.results["as_string"]
+    given = tck_util.result_to_set(result)
+    expected = tck_util.text_string_to_set(context.table)
+    if given != expected:
+        raise Exception("Strings does not match given: %s expected: %s" % (given, expected))
+
+
+@then("result should be relationship\(s\)")
+def step_impl(context):
+    result = context.results["as_string"]
+    given = tck_util.result_to_set(result)
+    expected = tck_util.text_relationship_to_set(context.table)
+    if given != expected:
+        raise Exception("Relationship does not match given: %s expected: %s" % (given, expected))
+
+
+@then("result should be empty")
+def step_impl(context):
+    assert context.results
+    for result in context.results.values():
+        assert len(result) == 0
+
+
+@then('result should be node "(?P<n1>.+)" node "(?P<n2>.+)" and int "(?P<len>\d+)"')
+def step_impl(context, n1, n2, len):
+    result = context.results["as_string"]
+    given = tck_util.single_result_to_values(result)
+    expected = tck_util.node_node_int_to_values(n1, n2, len)
+    if given != expected:
+        raise Exception("Mixed response does not match given: %s expected: %s" % (given, expected))
+
+
+@when('running "(?P<parameters>.+)": (?P<statement>.+);')
+def step_impl(context, parameters, statement):
+    parameters = json.loads(parameters)
+    context.results = {"as_string": tck_util.send_parameters(statement, parameters)}
--- test/tck/tck_util.py
+++ test/tck/tck_util.py
@@ -1,6 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Copyright (c) 2002-2016 "Neo Technology,"
+# Network Engine for Objects in Lund AB [http://neotechnology.com]
+#
+# This file is part of Neo4j.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import string
 import random
-from neo4j.v1 import compat
+import json
+from neo4j.v1 import compat, Relationship, Node, Path
 
 from neo4j.v1 import GraphDatabase
 
@@ -115,9 +136,234 @@ def get_dict_of_random_type(size, type):
     fu, args = _get_random_func(type)
     return {'a%d' % i: fu(*args) for i in range(size)}
 
+
 def to_unicode(val):
     try:
         return unicode(val)
     except NameError:
         return str(val)
+
+
+def get_properties(prop):
+    split_at = prop.find('{')
+    if split_at != -1:
+        n_start, prop = split_two(prop, split_at)
+        split_at = prop.find('}')
+        if split_at == -1:
+            raise ValueError("Node properties not correctly representetd. Found starrting '{' but no ending '}' in : %s" % properties)
+        prop, n_end = split_two(prop, split_at+1)
+        n = n_start + n_end
+        properties = json.loads(prop)
+        return properties, n
+    else:
+        return {}, prop
+
+
+def get_labels(labels):
+    labels = labels.split(':')
+    if len(labels) == 1:
+        return [], labels[0]
+    n = labels[0]
+    labels = labels[1:]
+    if labels[-1].find(' ') != -1:
+        labels[-1], n_end = split_two(labels[-1], labels[-1].find(' '))
+        n += n_end
+    return labels, n
+
+
+def get_labels_and_properties(n):
+    prop, n = get_properties(n)
+    labels, n = get_labels(n)
+    return labels, prop, n
+
+
+def split_two(string_entity, split_at):
+    return string_entity[:split_at], string_entity[split_at:]
+
+
+def get_node(string_entity):
+    if string_entity is None:
+        return None, ''
+    if string_entity[0] != "(":
+        raise ValueError("Excpected node which shuld start with '('. Got: %s" % string_entity[0])
+    split_at = string_entity.find(')')
+    if split_at == -1:
+        raise ValueError("Excpected node which shuld end with ')'. Found no such character in: %s" % string_entity)
+    n , string_entity = split_two(string_entity, split_at+1)
+    n = n[1:-1]
+    labels, properties, n = get_labels_and_properties(n)
+    node = Node(labels=labels, properties=properties)
+    return node, string_entity
+
+
+def get_relationship(string_entity):
+    point_up = None
+    if string_entity[:3] == "<-[":
+        point_up = False
+        string_entity = string_entity[3:]
+        rel = string_entity[:string_entity.find(']-')]
+        string_entity = string_entity[string_entity.find(']-')+2:]
+    elif string_entity[:2] == "-[":
+        point_up = True
+        string_entity = string_entity[2:]
+        rel = string_entity[:string_entity.find(']-')]
+        string_entity = string_entity[string_entity.find(']->')+3:]
+    elif string_entity[0] == "[":
+        string_entity = string_entity[1:]
+        rel = string_entity[:string_entity.find(']')]
+        string_entity = string_entity[string_entity.find(']')+1:]
+    else:
+        raise ValueError("Cant identify relationship from: %s" %string_entity)
+    type, properties, str = get_labels_and_properties(rel)
+    if len(type) > 1:
+        raise ValueError("Relationship can only have one type. Got: %s" % type)
+    relationship = Relationship(1, 2, type[0], properties=properties)
+    return relationship, string_entity, point_up
+
+
+def get_path(string_path):
+    n, string_path = get_node(string_path)
+    list_of_nodes_and_rel = [n]
+    n.identity = 0
+    while string_path != '':
+        r, string_path, point_up = get_relationship(string_path)
+        n, string_path = get_node(string_path)
+        n.identity = len(list_of_nodes_and_rel)
+        if point_up:
+            r.start = list_of_nodes_and_rel[-1].identity
+            r.end = n.identity
+        else:
+            r.start = n.identity
+            r.end = list_of_nodes_and_rel[-1].identity
+        list_of_nodes_and_rel.append(r)
+        list_of_nodes_and_rel.append(n)
+    path = Path(list_of_nodes_and_rel[0], *list_of_nodes_and_rel[1:])
+    return path, string_path
+
+
+def result_to_set(result, func=None, rel=False):
+    s = set([])
+    for row in result:
+        l = []
+        for i,_ in enumerate(row):
+            cell = row[i]
+            if cell == 'null':
+                cell = None
+            if isinstance(cell, compat.string) and cell[0] == '[':
+                if rel and cell[1] != '[':
+                    cell = [cell]
+                else:
+                    cell = cell[1:-1].split(", ")
+            if not isinstance(cell,list):
+                cell = [cell]
+            if func is not None:
+                cell = [Value(func(val)[0]) for val in cell]
+            else:
+                cell = [Value(val) for val in cell]
+            l.append(tuple(cell))
+        s.add(tuple(l))
+    return s
+
+
+def text_path_to_set(table):
+    return result_to_set(table, get_path)
+
+
+def text_relationship_to_set(table):
+    return result_to_set(table, get_relationship, rel=True)
+
+
+def text_node_to_set(table):
+    return result_to_set(table, get_node)
+
+
+def text_int_to_set(table):
+    def to_int(a):
+        return int(a), ''
+    return result_to_set(table, to_int)
+
+
+def text_string_to_set(table):
+    return result_to_set(table)
+
+
+def single_result_to_values(result):
+    l = []
+
+    for val in result[0].values():
+        if not isinstance(val,list):
+            val = [val]
+        val = [Value(v) for v in val]
+        l.append(tuple(val))
+    return tuple(l)
+
+
+def node_node_int_to_values(n1, n2, len):
+    n1 = tuple([Value(get_node(n1)[0])])
+    n2 = tuple([Value(get_node(n2)[0])])
+    len = tuple([Value(int(len))])
+    return tuple([n1,n2,len])
+
+
+class Value:
+    content = None
+
+    def __init__(self, entity):
+        self.content = {}
+        if isinstance(entity, Node):
+            self.content = self.create_node(entity)
+        elif isinstance(entity, Relationship):
+            self.content = self.create_relationship(entity)
+        elif isinstance(entity, Path):
+            self.content = self.create_path(entity)
+        elif isinstance(entity, int) or isinstance(entity, float) or isinstance(entity, (str, compat.string)) or entity is None:
+            self.content['value'] = entity
+        else:
+            raise ValueError("Do not support object type: %s" %entity)
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __eq__(self, other):
+        assert isinstance(other, Value)
+        return self.content == other.content
+
+    def __repr__(self):
+        return str(self.content)
+
+    def create_node(self,entity):
+        content = {'properties': entity.properties, 'labels': entity.labels, 'obj': "node"}
+
+        return content
+
+    def create_path(self,entity):
+        content = {}
+        prev_id = entity.start.identity
+        p = []
+        for i, rel in enumerate(list(entity)):
+            n = entity.nodes[i+1]
+            current_id = n.identity
+            if rel.start == prev_id and rel.end == current_id:
+                rel.start = i
+                rel.end = i+1
+            elif rel.start == current_id and rel.end == prev_id:
+                rel.start = i+1
+                rel.end = i
+            else:
+                raise ValueError("Relationships end and start should point to surrounding nodes. Rel: %s N1id: %s N2id: %s. At entity#%s" % (rel, current_id, prev_id, i))
+            p += [self.create_relationship(rel, True),self.create_node(n)]
+            prev_id = current_id
+        content['path'] = p
+        content['obj'] = "path"
+        content['start'] = self.create_node(entity.start)
+        return content
+
+    def create_relationship(self,entity, include_start_end=False):
+        content = {'obj': "relationship"}
+        if include_start_end:
+            self.content['start'] = entity.start
+            self.content['end'] = entity.end
+        content['type'] = entity.type
+        content['properties'] = entity.properties
+        return content
 
