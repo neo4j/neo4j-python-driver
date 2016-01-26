f22dc4387ccfbe7b1c3443a2b2ed8b5cae5492d3
RagnarW <ragnar.wernersson@neotechnology.com>
2016-01-26 15:12:45 +0100

    Removed unessessart imorts

--- test/tck/environment.py
+++ test/tck/environment.py
@@ -18,9 +18,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-
-from behave.log_capture import capture
 from test.tck import tck_util
 
 
