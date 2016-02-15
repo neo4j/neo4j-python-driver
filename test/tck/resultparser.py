bb758ab83185a209e0398fc7eff0b8e6edb0a258
RagnarW <ragnar.wernersson@neotechnology.com>
2016-02-15 16:29:53 +0100

    Updated tests after new feature file design

---
+++ test/tck/resultparser.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Copyright (c) 2002-2016 "Neo Technology,"
+# Network Engine for Objects in Lund AB [http://neotechnology.com]
+#
+# This file is part of Neo4j.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import re
+from neo4j.v1 import Node, Relationship, Path
+from tck_util import Value
+
+
+def parse_values_to_comparable(row):
+    return _parse(row, True)
+
+
+def parse_values(row):
+    return _parse(row, False)
+
+
+def _parse(row, comparable=False):
+    if is_array(row):
+        values = get_array(row)
+        result = []
+        for val in values:
+            result.append(_parse(val, comparable))
+        return result
+    elif is_map(row):
+        if comparable:
+            raise ValueError("No support for pasing maps of Node/Rels/paths atm")
+        return get_map(row)
+    else:
+        if comparable:
+            return value_to_comparable_object(row)
+        else:
+            return value_to_object(row)
+
+
+def is_array(val):
+    return val[0] == '[' and val[-1] == ']' and val[1] != ':'
+
+
+def get_array(val):
+    return val[1:-1].split(',')
+
+
+def is_map(val):
+    return val[0] == '{' and val[-1] == '}'
+
+
+def get_map(val):
+    return json.loads(val)
+
+
+def value_to_comparable_object(val):
+    return Value(value_to_object(val))
+
+
+def value_to_object(val):
+    val = val.strip()
+    print("VAL")
+    print(val)
+    PATH = '^(<\().*(\)>)$'
+    NODE = '^(\().*(\))$'
+    RELATIONSHIP = '^(\[:).*(\])$'
+    INTEGER = '^(-?[0-9]+)$'
+    STRING = '^(").*(")$'
+    NULL = '^null$'
+    BOOLEAN = '^(false|true)$'
+    # TEST FLOAT AFTER INT
+    FLOAT = '^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$'
+    if re.match(PATH, val):
+        return get_path(val)[0]
+    if re.match(RELATIONSHIP, val):
+        return get_relationship(val)[0]
+    if re.match(NODE, val):
+        return get_node(val)[0]
+    if re.match(NULL, val):
+        return None
+    if re.match(BOOLEAN, val):
+        return bool(val)
+    if re.match(STRING, val):
+        return val[1:-1]
+    if re.match(INTEGER, val):
+        return int(val)
+    if re.match(FLOAT, val):
+        return float(val)
+    raise TypeError("String value does not match any type. Got: %s" % val)
+
+
+def get_properties(prop):
+    split_at = prop.find('{')
+    if split_at != -1:
+        n_start, prop = split_two(prop, split_at)
+        split_at = prop.find('}')
+        if split_at == -1:
+            raise ValueError(
+                    "Node properties not correctly representetd. Found starrting '{' but no ending '}' in : %s" % prop)
+        prop, n_end = split_two(prop, split_at + 1)
+        n = n_start + n_end
+        properties = json.loads(prop)
+        return properties, n
+    else:
+        return {}, prop
+
+
+def get_labels(labels):
+    labels = labels.split(':')
+    if len(labels) == 1:
+        return [], labels[0]
+    n = labels[0]
+    labels = labels[1:]
+    if labels[-1].find(' ') != -1:
+        labels[-1], n_end = split_two(labels[-1], labels[-1].find(' '))
+        n += n_end
+    return labels, n
+
+
+def get_labels_and_properties(n):
+    prop, n = get_properties(n)
+    labels, n = get_labels(n)
+    return labels, prop, n
+
+
+def split_two(string_entity, split_at):
+    return string_entity[:split_at], string_entity[split_at:]
+
+
+def get_node(string_entity):
+    if string_entity[0] != "(":
+        raise ValueError("Excpected node which shuld start with '('. Got: %s" % string_entity[0])
+    split_at = string_entity.find(')')
+    if split_at == -1:
+        raise ValueError("Excpected node which shuld end with ')'. Found no such character in: %s" % string_entity)
+    n, string_entity = split_two(string_entity, split_at + 1)
+    n = n[1:-1]
+    labels, properties, n = get_labels_and_properties(n)
+    node = Node(labels=labels, properties=properties)
+    return node, string_entity
+
+
+def get_relationship(string_entity):
+    point_up = None
+    if string_entity[:3] == "<-[":
+        point_up = False
+        string_entity = string_entity[3:]
+        rel = string_entity[:string_entity.find(']-')]
+        string_entity = string_entity[string_entity.find(']-') + 2:]
+    elif string_entity[:2] == "-[":
+        point_up = True
+        string_entity = string_entity[2:]
+        rel = string_entity[:string_entity.find(']-')]
+        string_entity = string_entity[string_entity.find(']->') + 3:]
+    elif string_entity[0] == "[":
+        string_entity = string_entity[1:]
+        rel = string_entity[:string_entity.find(']')]
+        string_entity = string_entity[string_entity.find(']') + 1:]
+    else:
+        raise ValueError("Cant identify relationship from: %s" % string_entity)
+    type, properties, str = get_labels_and_properties(rel)
+    if len(type) > 1:
+        raise ValueError("Relationship can only have one type. Got: %s" % type)
+    relationship = Relationship(1, 2, type[0], properties=properties)
+    return relationship, string_entity, point_up
+
+
+def get_path(string_path):
+    id = 0
+    string_path = string_path[1:-1]
+    n, string_path = get_node(string_path)
+    list_of_nodes_and_rel = [n]
+    n.identity = ++id
+    while string_path != '':
+        r, string_path, point_up = get_relationship(string_path)
+        n, string_path = get_node(string_path)
+        n.identity = ++id
+        if point_up:
+            r.start = list_of_nodes_and_rel[-1].identity
+            r.end = n.identity
+            r.identity = 0
+        else:
+            r.start = n.identity
+            r.end = list_of_nodes_and_rel[-1].identity
+            r.identity = 0
+        list_of_nodes_and_rel.append(r)
+        list_of_nodes_and_rel.append(n)
+    path = Path(list_of_nodes_and_rel[0], *list_of_nodes_and_rel[1:])
+    return path, string_path
--- test/tck/steps/cypher_compability_steps.py
+++ test/tck/steps/cypher_compability_steps.py
@@ -18,219 +18,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-
 from behave import *
 
-from neo4j.v1 import compat, Node, Relationship, Path
-from test.tck.tck_util import Value, Type, string_to_type, send_string, send_parameters
+from test.tck.tck_util import Value, send_string, send_parameters
+from test.tck.resultparser import parse_values, parse_values_to_comparable
 
 use_step_matcher("re")
 
 
-@given("init: (?P<statement>.+);")
+@given("init: (?P<statement>.+)")
 def step_impl(context, statement):
     send_string(statement)
 
 
-@when("running: (?P<statement>.+);")
+@when("running: (?P<statement>.+)")
 def step_impl(context, statement):
     context.results = {"as_string": send_string(statement)}
 
 
-@then("result should be (?P<type>.+)\(s\)")
-def step_impl(context, type):
+@then("result")
+def step_impl(context):
     result = context.results["as_string"]
     given = driver_result_to_comparable_result(result)
-    expected = table_to_comparable_result(context.table, type)
+    expected = table_to_comparable_result(context.table)
     if not unordered_equal(given, expected):
-        raise Exception("%s does not match given: \n%s expected: \n%s" % (type, given, expected))
+        raise Exception("Does not match given: \n%s expected: \n%s" % (given, expected))
 
 
-@then("result should be empty")
-def step_impl(context):
-    assert context.results
-    for result in context.results.values():
-        assert len(result) == 0
-
-
-@then("result should map to types")
-def step_impl(context):
+@when('running parametrized: (?P<statement>.+)')
+def step_impl(context, statement):
+    assert len(context.table.rows) == 1
     keys = context.table.headings
     values = context.table.rows[0]
-    context.types = {keys[i]: values[i] for i in range(len(keys))}
-
-
-@then("result should be mixed")
-def step_impl(context):
-    result = context.results["as_string"]
-    given = driver_result_to_comparable_result(result)
-    expected = table_to_comparable_result(context.table, context.types)
-    if given != expected:
-        raise Exception("Mixed response does not match given: %s expected: %s" % (given, expected))
-
-
-@when('running parametrized: (?P<statement>.+);')
-def step_impl(context, statement):
-    parameters = {}
-    keys = context.table.headings
-    assert len(context.table.rows) == 1
-    values = context.table[0]
-    for i, v in enumerate(values):
-        if v[0] == '"':
-            val = v[1:-1]
-        else:
-            val = int(v)
-        parameters[keys[i]] = val
+    parameters = {keys[i]: parse_values(values[i]) for i in range(len(keys))}
 
     context.results = {"as_string": send_parameters(statement, parameters)}
 
 
-def get_properties(prop):
-    split_at = prop.find('{')
-    if split_at != -1:
-        n_start, prop = split_two(prop, split_at)
-        split_at = prop.find('}')
-        if split_at == -1:
-            raise ValueError(
-                    "Node properties not correctly representetd. Found starrting '{' but no ending '}' in : %s" % prop)
-        prop, n_end = split_two(prop, split_at + 1)
-        n = n_start + n_end
-        properties = json.loads(prop)
-        return properties, n
-    else:
-        return {}, prop
-
-
-def get_labels(labels):
-    labels = labels.split(':')
-    if len(labels) == 1:
-        return [], labels[0]
-    n = labels[0]
-    labels = labels[1:]
-    if labels[-1].find(' ') != -1:
-        labels[-1], n_end = split_two(labels[-1], labels[-1].find(' '))
-        n += n_end
-    return labels, n
-
-
-def get_labels_and_properties(n):
-    prop, n = get_properties(n)
-    labels, n = get_labels(n)
-    return labels, prop, n
-
-
-def split_two(string_entity, split_at):
-    return string_entity[:split_at], string_entity[split_at:]
-
-
-def get_node(string_entity):
-    if string_entity is None:
-        return None, ''
-    if string_entity[0] != "(":
-        raise ValueError("Excpected node which shuld start with '('. Got: %s" % string_entity[0])
-    split_at = string_entity.find(')')
-    if split_at == -1:
-        raise ValueError("Excpected node which shuld end with ')'. Found no such character in: %s" % string_entity)
-    n, string_entity = split_two(string_entity, split_at + 1)
-    n = n[1:-1]
-    labels, properties, n = get_labels_and_properties(n)
-    node = Node(labels=labels, properties=properties)
-    return node, string_entity
-
-
-def get_relationship(string_entity):
-    point_up = None
-    if string_entity[:3] == "<-[":
-        point_up = False
-        string_entity = string_entity[3:]
-        rel = string_entity[:string_entity.find(']-')]
-        string_entity = string_entity[string_entity.find(']-') + 2:]
-    elif string_entity[:2] == "-[":
-        point_up = True
-        string_entity = string_entity[2:]
-        rel = string_entity[:string_entity.find(']-')]
-        string_entity = string_entity[string_entity.find(']->') + 3:]
-    elif string_entity[0] == "[":
-        string_entity = string_entity[1:]
-        rel = string_entity[:string_entity.find(']')]
-        string_entity = string_entity[string_entity.find(']') + 1:]
-    else:
-        raise ValueError("Cant identify relationship from: %s" % string_entity)
-    type, properties, str = get_labels_and_properties(rel)
-    if len(type) > 1:
-        raise ValueError("Relationship can only have one type. Got: %s" % type)
-    relationship = Relationship(1, 2, type[0], properties=properties)
-    return relationship, string_entity, point_up
-
-
-def get_path(string_path):
-    n, string_path = get_node(string_path)
-    list_of_nodes_and_rel = [n]
-    n.identity = 0
-    while string_path != '':
-        r, string_path, point_up = get_relationship(string_path)
-        n, string_path = get_node(string_path)
-        n.identity = len(list_of_nodes_and_rel)
-        if point_up:
-            r.start = list_of_nodes_and_rel[-1].identity
-            r.end = n.identity
-        else:
-            r.start = n.identity
-            r.end = list_of_nodes_and_rel[-1].identity
-        list_of_nodes_and_rel.append(r)
-        list_of_nodes_and_rel.append(n)
-    path = Path(list_of_nodes_and_rel[0], *list_of_nodes_and_rel[1:])
-    return path, string_path
-
-
-def _string_value_to_comparable(val, type):
-    def get_val(v):
-        if type == Type.INTEGER:
-            return Value(int(v))
-        elif type == Type.STRING:
-            return Value(v)
-        elif type == Type.NODE:
-            return Value(get_node(v)[0])
-        elif type == Type.RELATIONSHIP:
-            return Value(get_relationship(v)[0])
-        elif type == Type.PATH:
-            return Value(get_path(v)[0])
-        else:
-            raise ValueError("Not recognized type: %s" % type)
-
-    assert isinstance(type, compat.string)
-    if val == 'null':
-        return Value(None)
-    if val[0] == '[':
-        if type != Type.RELATIONSHIP or (type == Type.RELATIONSHIP and val[1] == '['):
-            val = val[1:-1].split(", ")
-    if isinstance(val, list):
-        return tuple([get_val(v) for v in val])
-    else:
-        return get_val(val)
-
-
 def _driver_value_to_comparable(val):
     if isinstance(val, list):
-        return tuple([Value(v) for v in val])
+        l = [_driver_value_to_comparable(v) for v in val]
+        return l
     else:
         return Value(val)
 
 
-def table_to_comparable_result(table, types):
+def table_to_comparable_result(table):
     result = []
     keys = table.headings
-    if isinstance(types, compat.string):
-        types = {key: string_to_type(types) for key in keys}
-    elif isinstance(types, dict):
-        assert set(types.keys()) == set(keys)
-    else:
-        raise ValueError("types must be either string of single type or map of types corresponding to result keys Got:"
-                         " %s" % types)
     for row in table:
         result.append(
-                {keys[i]: _string_value_to_comparable(row[i], string_to_type(types[keys[i]])) for i in range(len(row))})
+                {keys[i]: parse_values_to_comparable(row[i]) for i in range(len(row))})
     return result
 
 
@@ -241,15 +79,19 @@ def driver_result_to_comparable_result(result):
     return records
 
 
-def unordered_equal(one, two):
-    l1 = one[:]
-    l2 = two[:]
+def unordered_equal(given, expected):
+    l1 = given[:]
+    l2 = expected[:]
     assert isinstance(l1, list)
     assert isinstance(l2, list)
+    print(len(l1))
+    print(len(l2))
     assert len(l1) == len(l2)
     for d1 in l1:
         size = len(l2)
         for d2 in l2:
+            print(d2)
+            print(d1)
             if d1 == d2:
                 l2.remove(d2)
                 break
