5694ec21da816ad475d7d370186cfc5e114f2df6
Nigel Small <nigel.small@neotechnology.com>
2016-05-31 13:48:17 +0100

    Updated imports in test file

--- test/test_session.py
+++ test/test_session.py
@@ -26,7 +26,7 @@ from unittest import skipUnless
 from mock import patch
 
 from neo4j.v1.constants import TRUST_ON_FIRST_USE
-from neo4j.v1.exceptions import CypherError, ResultError
+from neo4j.v1.exceptions import CypherError, ProtocolError, ResultError
 from neo4j.v1.session import GraphDatabase, basic_auth, Record, SSL_AVAILABLE
 from neo4j.v1.types import Node, Relationship, Path
 
@@ -34,7 +34,6 @@ from test.util import ServerTestCase
 
 
 auth_token = basic_auth("neo4j", "neo4j")
-from neo4j.v1.exceptions import ProtocolError
 
 
 class DriverTestCase(ServerTestCase):
