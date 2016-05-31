1b184120a2dab4e61156809ad091c1a66e046b7e
Nigel Small <nigel.small@neotechnology.com>
2016-05-31 13:00:31 +0100

    Exception import

--- neo4j/v1/__init__.py
+++ neo4j/v1/__init__.py
@@ -18,7 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .bolt import ProtocolError
 from .constants import *
+from .exceptions import *
 from .session import *
 from .types import *
