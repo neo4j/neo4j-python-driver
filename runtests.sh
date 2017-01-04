9dc242cc509263a23fbf13738f0eab9d6ea23255
Steven R. Baker <steven@stevenrbaker.com>
2017-01-04 12:04:33 +0100

    Override the default version list with environment.

--- runtests.sh
+++ runtests.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 RUN=$(dirname "$0")/test/run/
-VERSIONS="3.0.8:3.1.0"
+versions="${VERSIONS:-3.0.8:3.1.0}"
 
 # Export DIST_HOST=localhost if local web server hosts server packages
 if [ -z $1 ]
 then
     # Full test (with coverage)
-    neotest -e ${VERSIONS} ${RUN} coverage run --source neo4j -m unittest discover -vs test && coverage report --show-missing
+    neotest -e ${versions} ${RUN} coverage run --source neo4j -m unittest discover -vs test && coverage report --show-missing
 else
     # Partial test
-    neotest -e ${VERSIONS} ${RUN} coverage run --source neo4j -m unittest -v $1 && coverage report --show-missing
+    neotest -e ${versions} ${RUN} coverage run --source neo4j -m unittest -v $1 && coverage report --show-missing
 fi
