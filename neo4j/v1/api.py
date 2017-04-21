c42a822818370dfadc3bd6bb5f9cc12d39fffba7
Steven R. Baker <steven@stevenrbaker.com>
2017-04-21 12:00:11 +0200

    `clock()` gives CPU time, which is incorrect here.

--- neo4j/v1/api.py
+++ neo4j/v1/api.py
@@ -22,7 +22,7 @@
 from collections import deque
 from random import random
 from threading import RLock
-from time import clock, sleep
+from time import time, sleep
 from warnings import warn
 
 from neo4j.bolt import ProtocolError, ServiceUnavailable
@@ -425,7 +425,7 @@ class Session(object):
                                             RETRY_DELAY_MULTIPLIER,
                                             RETRY_DELAY_JITTER_FACTOR)
         last_error = None
-        t0 = t1 = clock()
+        t0 = t1 = time()
         while t1 - t0 <= self._max_retry_time:
             try:
                 self._create_transaction()
@@ -436,7 +436,7 @@ class Session(object):
             except (ServiceUnavailable, SessionExpired) as error:
                 last_error = error
             sleep(next(retry_delay))
-            t1 = clock()
+            t1 = time()
         raise last_error
 
     def read_transaction(self, unit_of_work, *args, **kwargs):
