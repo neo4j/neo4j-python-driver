0aa3d59123c50bd24e0d8bbd82f3897a7c8df1b0
Zhen <zhen.li@neotechnology.com>
2017-12-22 16:45:27 +0100

    Fixed a test failure due to message format changes in newer cypher versions

--- test/integration/test_session.py
+++ test/integration/test_session.py
@@ -222,9 +222,7 @@ class SummaryTestCase(DirectIntegrationTestCase):
                                                "different parts or by using OPTIONAL MATCH " \
                                                "(identifier is: (m))"
             position = notification.position
-            assert position.offset == 0
-            assert position.line == 1
-            assert position.column == 1
+            assert position
 
     def test_contains_time_information(self):
         if not self.at_least_version(3, 1):
