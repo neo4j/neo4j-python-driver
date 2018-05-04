b00722824be79dffae80731d0578e4734b2b549c
Nigel Small <nigel@neo4j.com>
2018-05-04 08:30:18 +0100

    Tweaked clean script

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@
 
 ROOT=$(dirname "$0")
 
-rm -rf ${ROOT}/build ${ROOT}/dist ${ROOT}/*.egg-info ${ROOT}/.coverage ${ROOT}/.tox ${ROOT}/.cache ${ROOT}/.pytest_cache ${ROOT}/.benchmarks
+rm -rf ${ROOT}/build ${ROOT}/dist ${ROOT}/docs/build ${ROOT}/*.egg-info ${ROOT}/.coverage ${ROOT}/.tox ${ROOT}/.cache ${ROOT}/.pytest_cache ${ROOT}/.benchmarks
 
 # These are removed to avoid name collision problems with snapshot server packages
 rm -rf ${ROOT}/test/integration/dist ${ROOT}/test/integration/run
