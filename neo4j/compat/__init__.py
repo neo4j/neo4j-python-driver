86882c1381bfee35fd4f1ae9fd129e5ab4c5f1f4
Nigel Small <nigel@neo4j.com>
2018-05-04 08:47:32 +0100

    Updated copyright

--- neo4j/compat/__init__.py
+++ neo4j/compat/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-# Copyright (c) 2002-2018 "Neo Technology,"
-# Network Engine for Objects in Lund AB [http://neotechnology.com]
+# Copyright (c) 2002-2018 Neo4j Sweden AB [http://neo4j.com]
 #
 # This file is part of Neo4j.
 #
