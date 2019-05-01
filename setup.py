d24a6f6d4ba17f79041dc0bcf4bdbaff6b0b581a
Nigel Small <nigel@neo4j.com>
2019-05-01 18:41:03 +0100

    Neobolt 1.7.8

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except ImportError:
 from neo4j.meta import package, version
 
 install_requires = [
-    "neobolt<2,>=1.7.7",
+    "neobolt<2,>=1.7.8",
     "neotime<2,>=1.7.1",
 ]
 classifiers = [
