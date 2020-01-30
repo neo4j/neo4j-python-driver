27db8107b80ecccd7d2e1300cefdb6fa6d2dd337
martin bendsoe <martin.bendsoe@neotechnology.com>
2020-01-30 13:21:26 +0100

    Parameterized test_should_be_able_to_write

--- tests/stub/test_routingdriver.py
+++ tests/stub/test_routingdriver.py
@@ -127,8 +127,16 @@ def test_should_be_able_to_read(driver_info, test_scripts):
                 assert result.summary().server.address == ('127.0.0.1', 9004)
 
 
-def test_should_be_able_to_write(driver_info):
-    with StubCluster("v3/router.script", "v3/create_a.script"):
+@pytest.mark.parametrize(
+    "test_scripts",
+    [
+        ("v3/router.script", "v3/create_a.script"),
+        ("v4x0/router.script", "v4x0/create_test_node_port_9006.script"),
+    ]
+)
+def test_should_be_able_to_write(driver_info, test_scripts):
+    # python -m pytest tests/stub/test_routingdriver.py -s -v -k test_should_be_able_to_write
+    with StubCluster(*test_scripts):
         uri = "bolt+routing://127.0.0.1:9001"
         with GraphDatabase.driver(uri, auth=driver_info["auth_token"]) as driver:
             with driver.session(default_access_mode=WRITE_ACCESS) as session:
