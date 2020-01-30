c43d9aa1f42a16dec8a6f76ef48ce5dcfeae8cc5
martin bendsoe <martin.bendsoe@neotechnology.com>
2020-01-30 17:24:28 +0100

    renamed stub script
    tests/stub/scripts/v4x0/tx_return_1_twice_port_9006.script
    
    to
    
    tests/stub/scripts/v4x0/tx_two_subsequent_return_1_port_9006.script

--- tests/stub/test_accesslevel.py
+++ tests/stub/test_accesslevel.py
@@ -152,7 +152,7 @@ def test_two_subsequent_read_transactions(driver_info, test_scripts):
     "test_scripts",
     [
         ("v3/router.script", "v3/return_1_in_write_tx_twice.script"),
-        ("v4x0/router.script", "v4x0/tx_return_1_twice_port_9006.script"),
+        ("v4x0/router.script", "v4x0/tx_two_subsequent_return_1_port_9006.script"),
     ]
 )
 def test_two_subsequent_write_transactions(driver_info, test_scripts):
