d040de4cd6aefd86126f62bb577bd1f72fc5b711
martin bendsoe <martin.bendsoe@neotechnology.com>
2020-04-14 10:01:53 +0200

    custom resolver example updated

--- tests/integration/examples/test_custom_resolver_example.py
+++ tests/integration/examples/test_custom_resolver_example.py
@@ -22,38 +22,47 @@
 import pytest
 
 # tag::custom-resolver-import[]
-from neo4j import GraphDatabase
+from neo4j import (
+    GraphDatabase,
+    WRITE_ACCESS,
+)
 # end::custom-resolver-import[]
 
 from neo4j.exceptions import ServiceUnavailable
 from neo4j._exceptions import BoltHandshakeError
-from tests.integration.examples import DriverSetupExample
 
 
 # python -m pytest tests/integration/examples/test_custom_resolver_example.py -s -v
 
+# tag::custom-resolver[]
+def create_driver(uri, user, password):
 
-class CustomResolverExample(DriverSetupExample):
+    def resolver(address):
+        host, port = address
+        if host == "x.example.com":
+            yield "a.example.com", port
+            yield "b.example.com", port
+            yield "c.example.com", port
+        else:
+            yield host, port
 
-    # tag::custom-resolver[]
-    def __init__(self, uri, auth):
+    return GraphDatabase.driver(uri, auth=(user, password), resolver=resolver)
 
-        def resolve(address):
-            host, port = address
-            if host == "x.example.com":
-                yield "a.example.com", port
-                yield "b.example.com", port
-                yield "c.example.com", port
-            else:
-                yield host, port
 
-        self.driver = GraphDatabase.driver(uri, auth=auth, resolver=resolve)
-    # end::custom-resolver[]
+def add_person(name):
+    driver = create_driver("neo4j://x.acme.com", user="neo4j", password="password")
+    session = driver.session(default_access_mode=WRITE_ACCESS)
+    session.run("CREATE (a:Person {name: $name})", {"name", name})
+    session.close()
+    driver.close()
+# end::custom-resolver[]
 
 
 def test_example(uri, auth):
     try:
-        CustomResolverExample.test(uri, auth)
+        add_person("testing_resolver")
     except ServiceUnavailable as error:
         if isinstance(error.__cause__, BoltHandshakeError):
             pytest.skip(error.args[0])
+    except ValueError as error:
+        assert error.args[0] == "Cannot resolve address x.acme.com:7687"
