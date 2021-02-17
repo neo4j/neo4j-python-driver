e15554e39969e41c1e45b4d2c96a5a0dd3d2595f
Rouven Bauer <rouven.bauer@neotechnology.com>
2021-02-17 09:16:30 +0100

    Remove year from the license header

--- tests/unit/io/test_class_bolt.py
+++ tests/unit/io/test_class_bolt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-# Copyright (c) 2002-2020 "Neo4j,"
+# Copyright (c) "Neo4j"
 # Neo4j Sweden AB [http://neo4j.com]
 #
 # This file is part of Neo4j.
