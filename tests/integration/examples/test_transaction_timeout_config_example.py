fc29c3898ef00bc6a9af44e95cba0ab64d4bdd7e
gjmwoods <42248895+gjmwoods@users.noreply.github.com>
2021-06-14 14:01:54 +0100

    Update tests/integration/examples/test_transaction_timeout_config_example.py
    Co-authored-by: Robsdedude <dev@rouvenbauer.de>

--- tests/integration/examples/test_transaction_timeout_config_example.py
+++ tests/integration/examples/test_transaction_timeout_config_example.py
@@ -26,7 +26,9 @@ from neo4j import unit_of_work, Query
 # tag::transaction-timeout-config[]
 @unit_of_work(timeout=5)
 def create_person(tx, name):
-    return tx.run("CREATE (a:Person {name: $name}) RETURN id(a)", name=name).single().value()
+    return tx.run(
+        "CREATE (a:Person {name: $name}) RETURN id(a)", name=name
+    ).single().value()
 
 
 def add_person(driver, name):
