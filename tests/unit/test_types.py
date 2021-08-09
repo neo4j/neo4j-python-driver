5ce2d54aba28da6173875894deee62accd7d705f
Rouven Bauer <rouven.bauer@neo4j.com>
2021-08-09 14:29:50 +0200

    4th round of migrating integration tests to TestKit

--- tests/unit/test_types.py
+++ tests/unit/test_types.py
@@ -18,8 +18,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from itertools import product
 
-from unittest import TestCase
+import pytest
 
 from neo4j.data import DataHydrator
 from neo4j.graph import (
@@ -39,14 +40,20 @@ from neo4j.packstream import Structure
 def test_can_create_node():
     g = Graph()
     gh = Graph.Hydrator(g)
-    alice = gh.hydrate_node(1, {"Person"}, {"name": "Alice", "age": 33})
+    alice = gh.hydrate_node(123, {"Person"}, {"name": "Alice", "age": 33})
     assert isinstance(alice, Node)
+    assert alice.id == 123
     assert alice.labels == {"Person"}
     assert set(alice.keys()) == {"name", "age"}
     assert set(alice.values()) == {"Alice", 33}
     assert set(alice.items()) == {("name", "Alice"), ("age", 33)}
+    assert dict(alice) == {"name": "Alice", "age": 33}
     assert alice.get("name") == "Alice"
     assert alice.get("age") == 33
+    assert alice.get("unknown") is None
+    assert alice.get("unknown", None) is None
+    assert alice.get("unknown", False) is False
+    assert alice.get("unknown", "default") == "default"
     assert len(alice) == 2
     assert alice["name"] == "Alice"
     assert alice["age"] == 33
@@ -55,10 +62,11 @@ def test_can_create_node():
     assert set(iter(alice)) == {"name", "age"}
 
 
-def test_null_properties():
+def test_node_with_null_properties():
     g = Graph()
     gh = Graph.Hydrator(g)
-    stuff = gh.hydrate_node(1, (), {"good": ["puppies", "kittens"], "bad": None})
+    stuff = gh.hydrate_node(1, (), {"good": ["puppies", "kittens"],
+                                    "bad": None})
     assert isinstance(stuff, Node)
     assert set(stuff.keys()) == {"good"}
     assert stuff.get("good") == ["puppies", "kittens"]
@@ -70,13 +78,26 @@ def test_null_properties():
     assert "bad" not in stuff
 
 
-def test_node_equality():
-    g = Graph()
-    node_1 = Node(g, 1234)
-    node_2 = Node(g, 1234)
-    node_3 = Node(g, 5678)
-    assert node_1 == node_2
-    assert node_1 != node_3
+@pytest.mark.parametrize(("g1", "id1", "props1", "g2", "id2", "props2"), (
+    (*n1, *n2)
+    for n1, n2 in product(
+        (
+            (g, id_, props)
+            for g in (0, 1)
+            for id_ in (1, 1234)
+            for props in (None, {}, {"a": 1})
+        ),
+        repeat=2
+    )
+))
+def test_node_equality(g1, id1, props1, g2, id2, props2):
+    graphs = (Graph(), Graph())
+    node_1 = Node(graphs[g1], id1, props1)
+    node_2 = Node(graphs[g2], id2, props2)
+    if g1 == g2 and id1 == id2:
+        assert node_1 == node_2
+    else:
+        assert node_1 != node_2
     assert node_1 != "this is not a node"
 
 
@@ -109,6 +130,7 @@ def test_can_create_relationship():
     assert alice_knows_bob.start_node == alice
     assert alice_knows_bob.type == "KNOWS"
     assert alice_knows_bob.end_node == bob
+    assert dict(alice_knows_bob) == {"since": 1999}
     assert set(alice_knows_bob.keys()) == {"since"}
     assert set(alice_knows_bob.values()) == {1999}
     assert set(alice_knows_bob.items()) == {("since", 1999)}
@@ -133,32 +155,41 @@ def test_can_create_path():
     alice = gh.hydrate_node(1, {"Person"}, {"name": "Alice", "age": 33})
     bob = gh.hydrate_node(2, {"Person"}, {"name": "Bob", "age": 44})
     carol = gh.hydrate_node(3, {"Person"}, {"name": "Carol", "age": 55})
-    alice_knows_bob = gh.hydrate_relationship(1, alice.id, bob.id, "KNOWS", {"since": 1999})
-    carol_dislikes_bob = gh.hydrate_relationship(2, carol.id, bob.id, "DISLIKES", {})
+    alice_knows_bob = gh.hydrate_relationship(1, alice.id, bob.id, "KNOWS",
+                                              {"since": 1999})
+    carol_dislikes_bob = gh.hydrate_relationship(2, carol.id, bob.id,
+                                                 "DISLIKES", {})
     path = Path(alice, alice_knows_bob, carol_dislikes_bob)
     assert isinstance(path, Path)
-    assert path.start_node == alice
-    assert path.end_node == carol
+    assert path.start_node is alice
+    assert path.end_node is carol
     assert path.nodes == (alice, bob, carol)
     assert path.relationships == (alice_knows_bob, carol_dislikes_bob)
     assert list(path) == [alice_knows_bob, carol_dislikes_bob]
 
 
-def test_can_hydrate_path():
+@pytest.mark.parametrize("cyclic", (True, False))
+def test_can_hydrate_path(cyclic):
     g = Graph()
     gh = Graph.Hydrator(g)
     alice = gh.hydrate_node(1, {"Person"}, {"name": "Alice", "age": 33})
     bob = gh.hydrate_node(2, {"Person"}, {"name": "Bob", "age": 44})
-    carol = gh.hydrate_node(3, {"Person"}, {"name": "Carol", "age": 55})
+    if cyclic:
+        carol = alice
+    else:
+        carol = gh.hydrate_node(3, {"Person"}, {"name": "Carol", "age": 55})
     r = [gh.hydrate_unbound_relationship(1, "KNOWS", {"since": 1999}),
          gh.hydrate_unbound_relationship(2, "DISLIKES", {})]
     path = gh.hydrate_path([alice, bob, carol], r, [1, 1, -2, 2])
-    assert path.start_node == alice
-    assert path.end_node == carol
+    assert path.start_node is alice
+    assert path.end_node is carol
     assert path.nodes == (alice, bob, carol)
-    expected_alice_knows_bob = gh.hydrate_relationship(1, alice.id, bob.id, "KNOWS", {"since": 1999})
-    expected_carol_dislikes_bob = gh.hydrate_relationship(2, carol.id, bob.id, "DISLIKES", {})
-    assert path.relationships == (expected_alice_knows_bob, expected_carol_dislikes_bob)
+    expected_alice_knows_bob = gh.hydrate_relationship(1, alice.id, bob.id,
+                                                       "KNOWS", {"since": 1999})
+    expected_carol_dislikes_bob = gh.hydrate_relationship(2, carol.id, bob.id,
+                                                          "DISLIKES", {})
+    assert path.relationships == (expected_alice_knows_bob,
+                                  expected_carol_dislikes_bob)
     assert list(path) == [expected_alice_knows_bob, expected_carol_dislikes_bob]
 
 
