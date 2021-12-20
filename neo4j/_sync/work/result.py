24370b8ca686a47223727b3e091b63774d9396b3
Rouven Bauer <rouven.bauer@neo4j.com>
2021-12-20 18:16:58 +0100

    Track generated sync code

---
+++ neo4j/_sync/work/result.py
@@ -0,0 +1,379 @@
+# Copyright (c) "Neo4j"
+# Neo4j Sweden AB [http://neo4j.com]
+#
+# This file is part of Neo4j.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from collections import deque
+from warnings import warn
+
+from ..._async_compat.util import Util
+from ...data import DataDehydrator
+from ...work import ResultSummary
+from ..io import ConnectionErrorHandler
+
+
+class Result:
+    """A handler for the result of Cypher query execution. Instances
+    of this class are typically constructed and returned by
+    :meth:`.AyncSession.run` and :meth:`.Transaction.run`.
+    """
+
+    def __init__(self, connection, hydrant, fetch_size, on_closed,
+                 on_error):
+        self._connection = ConnectionErrorHandler(connection, on_error)
+        self._hydrant = hydrant
+        self._on_closed = on_closed
+        self._metadata = None
+        self._record_buffer = deque()
+        self._summary = None
+        self._bookmark = None
+        self._raw_qid = -1
+        self._fetch_size = fetch_size
+
+        # states
+        self._discarding = False    # discard the remainder of records
+        self._attached = False      # attached to a connection
+        # there are still more response messages we wait for
+        self._streaming = False
+        # there ar more records available to pull from the server
+        self._has_more = False
+        # the result has been fully iterated or consumed
+        self._closed = False
+
+    @property
+    def _qid(self):
+        if self._raw_qid == self._connection.most_recent_qid:
+            return -1
+        else:
+            return self._raw_qid
+
+    def _tx_ready_run(self, query, parameters, **kwargs):
+        # BEGIN+RUN does not carry any extra on the RUN message.
+        # BEGIN {extra}
+        # RUN "query" {parameters} {extra}
+        self._run(
+            query, parameters, None, None, None, None, **kwargs
+        )
+
+    def _run(
+        self, query, parameters, db, imp_user, access_mode, bookmarks,
+        **kwargs
+    ):
+        query_text = str(query)  # Query or string object
+        query_metadata = getattr(query, "metadata", None)
+        query_timeout = getattr(query, "timeout", None)
+
+        parameters = DataDehydrator.fix_parameters(dict(parameters or {}, **kwargs))
+
+        self._metadata = {
+            "query": query_text,
+            "parameters": parameters,
+            "server": self._connection.server_info,
+        }
+
+        def on_attached(metadata):
+            self._metadata.update(metadata)
+            # For auto-commit there is no qid and Bolt 3 does not support qid
+            self._raw_qid = metadata.get("qid", -1)
+            if self._raw_qid != -1:
+                self._connection.most_recent_qid = self._raw_qid
+            self._keys = metadata.get("fields")
+            self._attached = True
+
+        def on_failed_attach(metadata):
+            self._metadata.update(metadata)
+            self._attached = False
+            Util.callback(self._on_closed)
+
+        self._connection.run(
+            query_text,
+            parameters=parameters,
+            mode=access_mode,
+            bookmarks=bookmarks,
+            metadata=query_metadata,
+            timeout=query_timeout,
+            db=db,
+            imp_user=imp_user,
+            on_success=on_attached,
+            on_failure=on_failed_attach,
+        )
+        self._pull()
+        self._connection.send_all()
+        self._attach()
+
+    def _pull(self):
+        def on_records(records):
+            if not self._discarding:
+                self._record_buffer.extend(self._hydrant.hydrate_records(self._keys, records))
+
+        def on_summary():
+            self._attached = False
+            Util.callback(self._on_closed)
+
+        def on_failure(metadata):
+            self._attached = False
+            Util.callback(self._on_closed)
+
+        def on_success(summary_metadata):
+            self._streaming = False
+            has_more = summary_metadata.get("has_more")
+            self._has_more = bool(has_more)
+            if has_more:
+                return
+            self._metadata.update(summary_metadata)
+            self._bookmark = summary_metadata.get("bookmark")
+
+        self._connection.pull(
+            n=self._fetch_size,
+            qid=self._qid,
+            on_records=on_records,
+            on_success=on_success,
+            on_failure=on_failure,
+            on_summary=on_summary,
+        )
+        self._streaming = True
+
+    def _discard(self):
+        def on_summary():
+            self._attached = False
+            Util.callback(self._on_closed)
+
+        def on_failure(metadata):
+            self._metadata.update(metadata)
+            self._attached = False
+            Util.callback(self._on_closed)
+
+        def on_success(summary_metadata):
+            self._streaming = False
+            has_more = summary_metadata.get("has_more")
+            self._has_more = bool(has_more)
+            if has_more:
+                return
+            self._discarding = False
+            self._metadata.update(summary_metadata)
+            self._bookmark = summary_metadata.get("bookmark")
+
+        # This was the last page received, discard the rest
+        self._connection.discard(
+            n=-1,
+            qid=self._qid,
+            on_success=on_success,
+            on_failure=on_failure,
+            on_summary=on_summary,
+        )
+        self._streaming = True
+
+    def __iter__(self):
+        """Iterator returning Records.
+        :returns: Record, it is an immutable ordered collection of key-value pairs.
+        :rtype: :class:`neo4j.Record`
+        """
+        while self._record_buffer or self._attached:
+            if self._record_buffer:
+                yield self._record_buffer.popleft()
+            elif self._streaming:
+                self._connection.fetch_message()
+            elif self._discarding:
+                self._discard()
+                self._connection.send_all()
+            elif self._has_more:
+                self._pull()
+                self._connection.send_all()
+
+        self._closed = True
+
+    def _attach(self):
+        """Sets the Result object in an attached state by fetching messages from
+        the connection to the buffer.
+        """
+        if self._closed is False:
+            while self._attached is False:
+                self._connection.fetch_message()
+
+    def _buffer(self, n=None):
+        """Try to fill `self._record_buffer` with n records.
+
+        Might end up with more records in the buffer if the fetch size makes it
+        overshoot.
+        Might ent up with fewer records in the buffer if there are not enough
+        records available.
+        """
+        record_buffer = deque()
+        for record in self:
+            record_buffer.append(record)
+            if n is not None and len(record_buffer) >= n:
+                break
+        self._closed = False
+        if n is None:
+            self._record_buffer = record_buffer
+        else:
+            self._record_buffer.extend(record_buffer)
+
+    def _buffer_all(self):
+        """Sets the Result object in an detached state by fetching all records
+        from the connection to the buffer.
+        """
+        self._buffer()
+
+    def _obtain_summary(self):
+        """Obtain the summary of this result, buffering any remaining records.
+
+        :returns: The :class:`neo4j.ResultSummary` for this result
+        """
+        if self._summary is None:
+            if self._metadata:
+                self._summary = ResultSummary(
+                    self._connection.unresolved_address, **self._metadata
+                )
+            elif self._connection:
+                self._summary = ResultSummary(
+                    self._connection.unresolved_address,
+                    server=self._connection.server_info
+                )
+
+        return self._summary
+
+    def keys(self):
+        """The keys for the records in this result.
+
+        :returns: tuple of key names
+        :rtype: tuple
+        """
+        return self._keys
+
+    def consume(self):
+        """Consume the remainder of this result and return a :class:`neo4j.ResultSummary`.
+
+        Example::
+
+            def create_node_tx(tx, name):
+                result = tx.run(
+                    "CREATE (n:ExampleNode { name: $name }) RETURN n", name=name
+                )
+                record = result.single()
+                value = record.value()
+                info = result.consume()
+                return value, info
+
+            with driver.session() as session:
+                node_id, info = session.write_transaction(create_node_tx, "example")
+
+        Example::
+
+            def get_two_tx(tx):
+                result = tx.run("UNWIND [1,2,3,4] AS x RETURN x")
+                values = []
+                for record in result:
+                    if len(values) >= 2:
+                        break
+                    values.append(record.values())
+                # discard the remaining records if there are any
+                info = result.consume()
+                # use the info for logging etc.
+                return values, info
+
+            with driver.session() as session:
+                values, info = session.read_transaction(get_two_tx)
+
+        :returns: The :class:`neo4j.ResultSummary` for this result
+        """
+        if self._closed is False:
+            self._discarding = True
+            for _ in self:
+                pass
+
+        return self._obtain_summary()
+
+    def single(self):
+        """Obtain the next and only remaining record from this result if available else return None.
+        Calling this method always exhausts the result.
+
+        A warning is generated if more than one record is available but
+        the first of these is still returned.
+
+        :returns: the next :class:`neo4j.Record` or :const:`None` if none remain
+        :warns: if more than one record is available
+        """
+        # TODO in 5.0 replace with this code that raises an error if there's not
+        # exactly one record in the left result stream.
+        # self._buffer(2).
+        # if len(self._record_buffer) != 1:
+        #     raise SomeError("Expected exactly 1 record, found %i"
+        #                      % len(self._record_buffer))
+        # return self._record_buffer.popleft()
+        # TODO: exhausts the result with self.consume if there are more records.
+        records = Util.list(self)
+        size = len(records)
+        if size == 0:
+            return None
+        if size != 1:
+            warn("Expected a result with a single record, but this result contains %d" % size)
+        return records[0]
+
+    def peek(self):
+        """Obtain the next record from this result without consuming it.
+        This leaves the record in the buffer for further processing.
+
+        :returns: the next :class:`.Record` or :const:`None` if none remain
+        """
+        self._buffer(1)
+        if self._record_buffer:
+            return self._record_buffer[0]
+
+    def graph(self):
+        """Return a :class:`neo4j.graph.Graph` instance containing all the graph objects
+        in the result. After calling this method, the result becomes
+        detached, buffering all remaining records.
+
+        :returns: a result graph
+        :rtype: :class:`neo4j.graph.Graph`
+        """
+        self._buffer_all()
+        return self._hydrant.graph
+
+    def value(self, key=0, default=None):
+        """Helper function that return the remainder of the result as a list of values.
+
+        See :class:`neo4j.Record.value`
+
+        :param key: field to return for each remaining record. Obtain a single value from the record by index or key.
+        :param default: default value, used if the index of key is unavailable
+        :returns: list of individual values
+        :rtype: list
+        """
+        return [record.value(key, default) for record in self]
+
+    def values(self, *keys):
+        """Helper function that return the remainder of the result as a list of values lists.
+
+        See :class:`neo4j.Record.values`
+
+        :param keys: fields to return for each remaining record. Optionally filtering to include only certain values by index or key.
+        :returns: list of values lists
+        :rtype: list
+        """
+        return [record.values(*keys) for record in self]
+
+    def data(self, *keys):
+        """Helper function that return the remainder of the result as a list of dictionaries.
+
+        See :class:`neo4j.Record.data`
+
+        :param keys: fields to return for each remaining record. Optionally filtering to include only certain values by index or key.
+        :returns: list of dictionaries
+        :rtype: list
+        """
+        return [record.data(*keys) for record in self]
---
+++ tests/unit/sync/io/test_class_bolt3.py
@@ -0,0 +1,115 @@
+# Copyright (c) "Neo4j"
+# Neo4j Sweden AB [http://neo4j.com]
+#
+# This file is part of Neo4j.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import pytest
+
+from neo4j._sync.io._bolt3 import Bolt3
+from neo4j.conf import PoolConfig
+from neo4j.exceptions import ConfigurationError
+
+from ..._async_compat import (
+    MagicMock,
+    mark_sync_test,
+)
+
+
+@pytest.mark.parametrize("set_stale", (True, False))
+def test_conn_is_stale(fake_socket, set_stale):
+    address = ("127.0.0.1", 7687)
+    max_connection_lifetime = 0
+    connection = Bolt3(address, fake_socket(address), max_connection_lifetime)
+    if set_stale:
+        connection.set_stale()
+    assert connection.stale() is True
+
+
+@pytest.mark.parametrize("set_stale", (True, False))
+def test_conn_is_not_stale_if_not_enabled(fake_socket, set_stale):
+    address = ("127.0.0.1", 7687)
+    max_connection_lifetime = -1
+    connection = Bolt3(address, fake_socket(address), max_connection_lifetime)
+    if set_stale:
+        connection.set_stale()
+    assert connection.stale() is set_stale
+
+
+@pytest.mark.parametrize("set_stale", (True, False))
+def test_conn_is_not_stale(fake_socket, set_stale):
+    address = ("127.0.0.1", 7687)
+    max_connection_lifetime = 999999999
+    connection = Bolt3(address, fake_socket(address), max_connection_lifetime)
+    if set_stale:
+        connection.set_stale()
+    assert connection.stale() is set_stale
+
+
+def test_db_extra_not_supported_in_begin(fake_socket):
+    address = ("127.0.0.1", 7687)
+    connection = Bolt3(address, fake_socket(address), PoolConfig.max_connection_lifetime)
+    with pytest.raises(ConfigurationError):
+        connection.begin(db="something")
+
+
+def test_db_extra_not_supported_in_run(fake_socket):
+    address = ("127.0.0.1", 7687)
+    connection = Bolt3(address, fake_socket(address), PoolConfig.max_connection_lifetime)
+    with pytest.raises(ConfigurationError):
+        connection.run("", db="something")
+
+
+@mark_sync_test
+def test_simple_discard(fake_socket):
+    address = ("127.0.0.1", 7687)
+    socket = fake_socket(address)
+    connection = Bolt3(address, socket, PoolConfig.max_connection_lifetime)
+    connection.discard()
+    connection.send_all()
+    tag, fields = socket.pop_message()
+    assert tag == b"\x2F"
+    assert len(fields) == 0
+
+
+@mark_sync_test
+def test_simple_pull(fake_socket):
+    address = ("127.0.0.1", 7687)
+    socket = fake_socket(address)
+    connection = Bolt3(address, socket, PoolConfig.max_connection_lifetime)
+    connection.pull()
+    connection.send_all()
+    tag, fields = socket.pop_message()
+    assert tag == b"\x3F"
+    assert len(fields) == 0
+
+
+@pytest.mark.parametrize("recv_timeout", (1, -1))
+@mark_sync_test
+def test_hint_recv_timeout_seconds_gets_ignored(
+    fake_socket_pair, recv_timeout
+):
+    address = ("127.0.0.1", 7687)
+    sockets = fake_socket_pair(address)
+    sockets.client.settimeout = MagicMock()
+    sockets.server.send_message(0x70, {
+        "server": "Neo4j/3.5.0",
+        "hints": {"connection.recv_timeout_seconds": recv_timeout},
+    })
+    connection = Bolt3(
+        address, sockets.client, PoolConfig.max_connection_lifetime
+    )
+    connection.hello()
+    sockets.client.settimeout.assert_not_called()
