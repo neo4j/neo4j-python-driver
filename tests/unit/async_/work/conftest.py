8c6103cd0dc99f02dd56d82bd2bfc806eaebbc3d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-03 19:03:46 +0800

    Delete tests directory

--- tests/unit/async_/work/conftest.py
+++
@@ -1,4 +0,0 @@
-from ._fake_connection import (
-    async_fake_connection,
-    async_fake_connection_generator,
-)
--- tests/unit/sync/work/conftest.py
+++
@@ -1,4 +0,0 @@
-from ._fake_connection import (
-    fake_connection,
-    fake_connection_generator,
-)
