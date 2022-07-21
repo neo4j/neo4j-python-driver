6bdf13b92ad35aaad9bf147d5de835861bc391a3
IvanPrychantovskyi <109072205+IvanPrychantovskyi@users.noreply.github.com>
2022-07-21 15:08:30 +0100

    Add files via upload

---
+++ test_wgs84_point.py
@@ -0,0 +1,62 @@
+# Copyright (c) "Neo4j"
+# Neo4j Sweden AB [https://neo4j.com]
+#
+# This file is part of Neo4j.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import pytest
+
+from neo4j.spatial import WGS84Point
+
+
+class WGS84PointTestCase:
+
+    def test_alias_3d(self):
+        x, y, z = 3.2, 4.0, -1.2
+        p = WGS84Point((x, y, z))
+
+        assert hasattr(p, "longitude")
+        assert p.longitude == x
+        assert hasattr(p, "x")
+        assert p.x == x
+
+        assert hasattr(p, "latitude")
+        assert p.latitude == y
+        assert hasattr(p, "y")
+        assert p.y == y
+
+        assert hasattr(p, "height")
+        assert p.height == z
+        assert hasattr(p, "z")
+        assert p.z == z
+
+    def test_alias_2d(self):
+        x, y = 3.2, 4.0
+        p = WGS84Point((x, y))
+
+        assert hasattr(p, "longitude")
+        assert p.longitude == x
+        assert hasattr(p, "x")
+        assert p.x == x
+
+        assert hasattr(p, "latitude")
+        assert p.latitude == y
+        assert hasattr(p, "y")
+        assert p.y == y
+
+        with pytest.raises(AttributeError):
+            p.height
+        with pytest.raises(AttributeError):
+            p.z
