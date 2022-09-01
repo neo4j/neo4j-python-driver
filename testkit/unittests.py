50ceb59bce2a010a45d1e6aa10f84808526542db
Rouven Bauer <rouven.bauer@neo4j.com>
2022-09-01 15:17:10 +0200

    Improve TestKit glue output

--- testkit/unittests.py
+++ testkit/unittests.py
@@ -1,9 +1,12 @@
 import subprocess
+import sys
 
 
 def run(args):
     subprocess.run(
-        args, universal_newlines=True, stderr=subprocess.STDOUT, check=True)
+        args, universal_newlines=True, stdout=sys.stdout, stderr=sys.stderr,
+        check=True
+    )
 
 
 if __name__ == "__main__":
