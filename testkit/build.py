7c5777674ee9d3b886a64213f435df4c0737e305
Robsdedude <rouven.bauer@neo4j.com>
2022-09-22 17:25:13 +0200

    Improve TestKit glue
     * add option to choose interpreter version
     * make Python invocations more strict (warnings as errors)
     * refactor and simplify code

--- testkit/build.py
+++ testkit/build.py
@@ -4,17 +4,11 @@ Responsible for building driver and test backend.
 """
 
 
-import subprocess
-import sys
-
-
-def run(args, env=None):
-    subprocess.run(args, universal_newlines=True, stdout=sys.stdout,
-                   stderr=sys.stderr, check=True, env=env)
+from _common import run_python
 
 
 if __name__ == "__main__":
-    run(["python", "setup.py", "build"])
-    run(["python", "-m", "pip", "install", "-U", "pip"])
-    run(["python", "-m", "pip", "install", "-Ur",
-         "testkitbackend/requirements.txt"])
+    run_python(["setup.py", "build"])
+    run_python(["-m", "pip", "install", "-U", "pip"])
+    run_python(["-m", "pip", "install", "-Ur",
+                "testkitbackend/requirements.txt"])
--- testkit/unittests.py
+++ testkit/unittests.py
@@ -1,14 +1,25 @@
-import subprocess
-import sys
+#!/usr/bin/env python
+
+# Copyright (c) "Neo4j"
+# Neo4j Sweden AB [https://neo4j.com]
+#
+# This file is part of Neo4j.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
-def run(args):
-    subprocess.run(
-        args, universal_newlines=True, stdout=sys.stdout, stderr=sys.stderr,
-        check=True
-    )
+from _common import run_python
 
 
 if __name__ == "__main__":
-    run([
-        "python", "-m", "tox", "-c", "tox-unit.ini"])
+    run_python(["-m", "tox", "-c", "tox-unit.ini"])
