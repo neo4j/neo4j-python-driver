9ce1abfde1e3466e7c25009cb06b28d5b1ae0553
Robsdedude <rouven.bauer@neo4j.com>
2023-01-12 11:16:39 +0100

    Bump version to 5.4

--- src/neo4j/_meta.py
+++ src/neo4j/_meta.py
@@ -28,7 +28,7 @@ _FuncT = t.TypeVar("_FuncT", bound=t.Callable)
 
 # Can be automatically overridden in builds
 package = "neo4j"
-version = "5.3.dev0"
+version = "5.4.dev0"
 deprecated_package = False
 
 
