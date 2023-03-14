6e8457bab50c4e59d35c5ae6feec569a16b50006
Robsdedude <rouven.bauer@neo4j.com>
2023-03-14 17:05:31 +0100

    Silence mypy error (#903)
    Staring with mypy 1.1.1, mypy started complaining about `__new__` of for example
    `Tuple[float, ...]`, which we are using in spatial and temporal types.

--- src/neo4j/_spatial/__init__.py
+++ src/neo4j/_spatial/__init__.py
@@ -60,7 +60,10 @@ class Point(t.Tuple[float, ...]):
         def z(self) -> float: ...
 
     def __new__(cls, iterable: t.Iterable[float]) -> Point:
-        return tuple.__new__(cls, map(float, iterable))
+        # mypy issue https://github.com/python/mypy/issues/14890
+        return tuple.__new__(  # type: ignore[type-var]
+            cls, map(float, iterable)
+        )
 
     def __repr__(self) -> str:
         return "POINT(%s)" % " ".join(map(str, self))
