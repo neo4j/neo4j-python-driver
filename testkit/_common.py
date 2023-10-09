03d52efc7d11730866008e79c4c828519d85188f
Robsdedude <rouven.bauer@neo4j.com>
2023-10-09 11:43:28 +0200

    Silence more deprecation warnings for Python 3.12

--- testkit/_common.py
+++ testkit/_common.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 import sys
 
@@ -7,15 +8,24 @@ TEST_BACKEND_VERSION = os.getenv("TEST_BACKEND_VERSION", "python")
 
 
 def run(args, env=None):
+    print(args)
     return subprocess.run(
         args, universal_newlines=True, stdout=sys.stdout, stderr=sys.stderr,
         check=True, env=env
     )
 
 
+def get_python_version():
+    cmd = [TEST_BACKEND_VERSION, "-V"]
+    res = subprocess.check_output(cmd, universal_newlines=True,
+                                  stderr=sys.stderr)
+    raw_version = re.match(r"(?:.*?)((?:\d+\.)+(?:\d+))", res).group(1)
+    return tuple(int(e) for e in raw_version.split("."))
+
+
 def run_python(args, env=None, warning_as_error=True):
     cmd = [TEST_BACKEND_VERSION, "-u"]
-    if sys.version_info >= (3, 12):
+    if get_python_version() >= (3, 12):
         # Ignore warnings for Python 3.12 for now
         # https://github.com/dateutil/dateutil/issues/1284 needs to be released
         # and propagate through our dependency graph
