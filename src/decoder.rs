8ee7a1437dcf5f507b74b9c727033fe4d018ba9c
Grant Lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2023-10-12 01:24:05 +0100

    fix some issues

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,5 +1,5 @@
 use pyo3::prelude::*;
-use pyo3::types::{PyDict, IntoPyDict};
+use pyo3::types::{PyDict, IntoPyDict, PyList};
 
 const NULL: u8 = 0xC0;
 const FALSE: u8 = 0xC2;
@@ -25,7 +25,7 @@ const TINY_STRUCT: u8 = 0xB0;
 const STRUCT_8: u8 = 0xDC;
 const STRUCT_16: u8 = 0xDD;
 
-struct PackStreamDecoder<'a> {
+pub(crate) struct PackStreamDecoder<'a> {
     bytes: Vec<u8>,
     py: Python<'a>,
     index: usize
@@ -43,14 +43,14 @@ impl <'b> PackStreamDecoder<'b> {
     }
 
     fn read_value(&mut self, marker: u8) -> PyObject {
-        let high_nibble = (marker & 0xF0).u8;
+        let high_nibble = (marker & 0xF0) as u8;
         
         match high_nibble {
             TINY_STRING => self.read_string((marker & 0x0F) as usize),
             TINY_LIST => self.read_list((marker & 0x0F) as usize),
             TINY_MAP => self.read_map((marker & 0x0F) as usize),
             _ => match marker {
-
+                _ => panic!("Invalid marker: {}", marker)
             }
         }
     }
@@ -70,7 +70,7 @@ impl <'b> PackStreamDecoder<'b> {
         if length == 0 {
             return "".to_object(self.py);
         }
-        let data = self.bytes[self.index..self.index + length];
+        let data = &self.bytes[self.index..self.index + length];
         self.index += length;
         return String::from_utf8(data).unwrap().to_object(self.py);
     }
@@ -86,13 +86,13 @@ impl <'b> PackStreamDecoder<'b> {
             let value = self.read();
             kvps.push(key, value);
         }
-        return kvps.into_py_dict(self.py);
+        return kvps.into_py_dict(self.py).into();
     }
 
     fn read_string_length(&mut self) -> usize {
         let marker = self.bytes[self.index];
         self.index += 1;
-        let high_nibble = (marker & 0xF0).u8;
+        let high_nibble = (marker & 0xF0) as u8;
         match high_nibble {
             TINY_STRING => (marker & 0x0F) as usize,
             STRING_8 => self.read_u8() as usize,
@@ -105,29 +105,29 @@ impl <'b> PackStreamDecoder<'b> {
     fn read_u8(&mut self) -> i32 {
         let value = self.bytes[self.index];
         self.index += 1;
-        return value & 0xFF;
+        return (value & 0xFF).into();
     }
 
     fn read_u16(&mut self) -> i32 {
-        let value = u16::from_be_bytes(self.bytes[self.index..self.index + 2usize   ]);
+        let value = u16::from_be_bytes(self.bytes[self.index..self.index + 2].try_into().unwrap());
         self.index += 2;
-        return value & 0xFFFF;
+        return (value & 0xFFFF).into();
     }
 
     fn read_u32(&mut self) -> i64 {
-        let value = u32::from_be_bytes(self.bytes[self.index..self.index + 4usize]);
+        let value = u32::from_be_bytes(self.bytes[self.index..self.index + 4].try_into().unwrap());
         self.index += 4;
-        return value & 0xFFFFFFFFu64;
+        return (value & 0xFFFFFFFF).into();
     }
 
     fn read_u64(&mut self) -> u64 {
-        let value = u64::from_be_bytes(self.bytes[self.index..self.index + 8usize]);
+        let value = u64::from_be_bytes(self.bytes[self.index..self.index + 8].try_into().unwrap());
         self.index += 8;
         return value;
     }
 
     fn read_double(&mut self) -> f64 {
-        let value = f64::from_be_bytes(self.bytes[self.index..self.index + 8usize]);
+        let value = f64::from_be_bytes(self.bytes[self.index..self.index + 8].try_into().unwrap());
         self.index += 8;
         return value;
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
+mod decoder;
+use decoder::PackStreamDecoder;
 use pyo3::prelude::*;
-use pyo3::types::{PyDict, IntoPyDict};
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn read_map(py: Python, bytes: Vec<u8>) -> PyResult<&PyDict> {
-    let mut kvps: Vec<(String, PyObject)> = Vec::new();
-    kvps.push(("Hello".to_string(), 10.to_object(py)));
-    kvps.push(("Hello2".to_string(), 10f32.to_object(py)));
-    kvps.push(("Hello3".to_string(), PyObject::from(PyDict::new(py))));
-    Ok(kvps.into_py_dict(py))
+fn read_map(py: Python, bytes: Vec<u8>) -> PyResult<&PyObject> {
+    let mut decoder = PackStreamDecoder::new(bytes, &py);
+    return Ok(&decoder.read());
 }
 
 /// A Python module implemented in Rust.
