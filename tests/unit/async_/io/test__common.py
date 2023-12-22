145df765cb634282c02aa0846db19ce5ab5848a1
Robsdedude <rouven.bauer@neo4j.com>
2023-12-22 13:05:57 +0100

    Add unit tests for ResetResponse handler

--- tests/unit/async_/io/test__common.py
+++ tests/unit/async_/io/test__common.py
@@ -14,9 +14,14 @@
 # limitations under the License.
 
 
+import logging
+
 import pytest
 
-from neo4j._async.io._common import AsyncOutbox
+from neo4j._async.io._common import (
+    AsyncOutbox,
+    ResetResponse,
+)
 from neo4j._codec.packstream.v1 import PackableBuffer
 
 from ...._async_compat import mark_async_test
@@ -56,3 +61,103 @@ async def test_async_outbox_chunking(chunk_size, data, result, mocker):
 
     assert not await outbox.flush()
     socket_mock.sendall.assert_awaited_once()
+
+
+def get_handler_arg(response):
+    if response == "RECORD":
+        return []
+    elif response == "IGNORED":
+        return {}
+    elif response == "FAILURE":
+        return {}
+    elif response == "SUCCESS":
+        return {}
+    else:
+        raise ValueError(f"Unexpected response: {response}")
+
+
+def call_handler(handler, response, arg=None):
+    if arg is None:
+        arg = get_handler_arg(response)
+
+    if response == "RECORD":
+        return handler.on_records(arg)
+    elif response == "IGNORED":
+        return handler.on_ignored(arg)
+    elif response == "FAILURE":
+        return handler.on_failure(arg)
+    elif response == "SUCCESS":
+        return handler.on_success(arg)
+    else:
+        raise ValueError(f"Unexpected response: {response}")
+
+
+@pytest.mark.parametrize(
+    ("response", "unexpected"),
+    (
+        ("RECORD", True),
+        ("IGNORED", True),
+        ("FAILURE", True),
+        ("SUCCESS", False),
+    )
+)
+@mark_async_test
+async def test_reset_response_closes_connection_on_unexpected_responses(
+    response, unexpected, async_fake_connection
+):
+    handler = ResetResponse(async_fake_connection, "reset", {})
+    async_fake_connection.close.assert_not_called()
+
+    await call_handler(handler, response)
+
+    if unexpected:
+        async_fake_connection.close.assert_awaited_once()
+    else:
+        async_fake_connection.close.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    ("response", "unexpected"),
+    (
+        ("RECORD", True),
+        ("IGNORED", True),
+        ("FAILURE", True),
+        ("SUCCESS", False),
+    )
+)
+@mark_async_test
+async def test_reset_response_logs_warning_on_unexpected_responses(
+    response, unexpected, async_fake_connection, caplog
+):
+    handler = ResetResponse(async_fake_connection, "reset", {})
+
+    with caplog.at_level(logging.WARNING):
+        await call_handler(handler, response)
+
+    log_message_found = any("RESET" in msg and "unexpected response" in msg
+                            for msg in caplog.messages)
+    if unexpected:
+        assert log_message_found
+    else:
+        assert not log_message_found
+
+
+@pytest.mark.parametrize("response",
+                         ("RECORD", "IGNORED", "FAILURE", "SUCCESS"))
+@mark_async_test
+async def test_reset_response_never_calls_handlers(
+    response, async_fake_connection, mocker
+):
+    handlers = {
+        key: mocker.AsyncMock(name=key)
+        for key in
+        ("on_records", "on_ignored", "on_failure", "on_success", "on_summary")
+    }
+
+    handler = ResetResponse(async_fake_connection, "reset", {}, **handlers)
+
+    arg = get_handler_arg(response)
+    await call_handler(handler, response, arg)
+
+    for handler in handlers.values():
+        handler.assert_not_called()
--- tests/unit/sync/io/test__common.py
+++ tests/unit/sync/io/test__common.py
@@ -14,10 +14,15 @@
 # limitations under the License.
 
 
+import logging
+
 import pytest
 
 from neo4j._codec.packstream.v1 import PackableBuffer
-from neo4j._sync.io._common import Outbox
+from neo4j._sync.io._common import (
+    Outbox,
+    ResetResponse,
+)
 
 from ...._async_compat import mark_sync_test
 
@@ -56,3 +61,103 @@ def test_async_outbox_chunking(chunk_size, data, result, mocker):
 
     assert not outbox.flush()
     socket_mock.sendall.assert_called_once()
+
+
+def get_handler_arg(response):
+    if response == "RECORD":
+        return []
+    elif response == "IGNORED":
+        return {}
+    elif response == "FAILURE":
+        return {}
+    elif response == "SUCCESS":
+        return {}
+    else:
+        raise ValueError(f"Unexpected response: {response}")
+
+
+def call_handler(handler, response, arg=None):
+    if arg is None:
+        arg = get_handler_arg(response)
+
+    if response == "RECORD":
+        return handler.on_records(arg)
+    elif response == "IGNORED":
+        return handler.on_ignored(arg)
+    elif response == "FAILURE":
+        return handler.on_failure(arg)
+    elif response == "SUCCESS":
+        return handler.on_success(arg)
+    else:
+        raise ValueError(f"Unexpected response: {response}")
+
+
+@pytest.mark.parametrize(
+    ("response", "unexpected"),
+    (
+        ("RECORD", True),
+        ("IGNORED", True),
+        ("FAILURE", True),
+        ("SUCCESS", False),
+    )
+)
+@mark_sync_test
+def test_reset_response_closes_connection_on_unexpected_responses(
+    response, unexpected, fake_connection
+):
+    handler = ResetResponse(fake_connection, "reset", {})
+    fake_connection.close.assert_not_called()
+
+    call_handler(handler, response)
+
+    if unexpected:
+        fake_connection.close.assert_called_once()
+    else:
+        fake_connection.close.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    ("response", "unexpected"),
+    (
+        ("RECORD", True),
+        ("IGNORED", True),
+        ("FAILURE", True),
+        ("SUCCESS", False),
+    )
+)
+@mark_sync_test
+def test_reset_response_logs_warning_on_unexpected_responses(
+    response, unexpected, fake_connection, caplog
+):
+    handler = ResetResponse(fake_connection, "reset", {})
+
+    with caplog.at_level(logging.WARNING):
+        call_handler(handler, response)
+
+    log_message_found = any("RESET" in msg and "unexpected response" in msg
+                            for msg in caplog.messages)
+    if unexpected:
+        assert log_message_found
+    else:
+        assert not log_message_found
+
+
+@pytest.mark.parametrize("response",
+                         ("RECORD", "IGNORED", "FAILURE", "SUCCESS"))
+@mark_sync_test
+def test_reset_response_never_calls_handlers(
+    response, fake_connection, mocker
+):
+    handlers = {
+        key: mocker.MagicMock(name=key)
+        for key in
+        ("on_records", "on_ignored", "on_failure", "on_success", "on_summary")
+    }
+
+    handler = ResetResponse(fake_connection, "reset", {}, **handlers)
+
+    arg = get_handler_arg(response)
+    call_handler(handler, response, arg)
+
+    for handler in handlers.values():
+        handler.assert_not_called()
