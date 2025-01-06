19836b6fed5576755e2c0cd43d3860f2541fad1d
nissan-polyrize <58933312+nissan-polyrize@users.noreply.github.com>
2025-01-06 11:43:34 +0200

    Revert "Update _bolt5.py"
    This reverts commit 0dc25ecdfbb7c82dbdbd0c35be8317f092fb3b95.

--- src/neo4j/_sync/io/_bolt5.py
+++ src/neo4j/_sync/io/_bolt5.py
@@ -327,16 +327,15 @@ class Bolt5x0(Bolt):
         qid=-1,
         dehydration_hooks=None,
         hydration_hooks=None,
-        compressed=False,
         **handlers,
     ):
         dehydration_hooks, hydration_hooks = self._default_hydration_hooks(
             dehydration_hooks, hydration_hooks
         )
-        extra = {"n": n, "compressed": False}
+        extra = {"n": n}
         if qid != -1:
             extra["qid"] = qid
-        log.debug("[#%04X]  C: PULL %s%r", self.local_port, "(compressed) " if compressed else "", extra)
+        log.debug("[#%04X]  C: PULL %r", self.local_port, extra)
         self._append(
             b"\x3f",
             (extra,),
@@ -928,7 +927,6 @@ class Bolt5x5(Bolt5x4):
         notifications_disabled_classifications=None,
         dehydration_hooks=None,
         hydration_hooks=None,
-        compressed=False,
         **handlers,
     ):
         dehydration_hooks, hydration_hooks = self._default_hydration_hooks(
@@ -973,19 +971,9 @@ class Bolt5x5(Bolt5x4):
                 ) from None
         if timeout is not None:
             extra["tx_timeout"] = tx_timeout_as_ms(timeout)
-
-        if compressed:
-            import zlib
-            import json
-            extra["compressed"] = compressed
-            query = zlib.compress(query.encode("UTF-8"))
-            parameters = zlib.compress(json.dumps(parameters).encode("UTF-8"))
-            fields = (extra, query, parameters)
-        else:
-            fields = (query, parameters, extra)
-
+        fields = (query, parameters, extra)
         log.debug(
-            "[#%04X]  C: RUN %s%s", self.local_port, "(compressed)" if compressed else "", " ".join(map(repr, fields))
+            "[#%04X]  C: RUN %s", self.local_port, " ".join(map(repr, fields))
         )
         self._append(
             b"\x10",
