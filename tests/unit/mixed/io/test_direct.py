48be612a2a124d9cc6f9708cc82a6fa83dc6223a
Robsdedude <rouven.bauer@neo4j.com>
2025-01-13 10:14:00 +0100

    Tests: detect timeout when joining threads

--- tests/unit/mixed/io/test_direct.py
+++ tests/unit/mixed/io/test_direct.py
@@ -122,7 +122,9 @@ class TestMixedConnectionPoolTestCase:
 
             # wait for all threads to release connections back to pool
             for t in threads:
-                t.join(timeout=1)
+                t.join(timeout=5)
+                if t.is_alive():
+                    raise TimeoutError(f"Joining thread timed out: {t!r}")
             # The pool size is still 5, but all are free
             self.assert_pool_size(address, 0, 5, pool)
 
