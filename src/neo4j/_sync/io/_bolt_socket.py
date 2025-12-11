275c73883f4f1c3b61d16422ad5beb184a4a945d
Robsdedude <rouven.bauer@neo4j.com>
2025-12-11 09:51:17 +0100

    Fix: connection_acquisition_timeout now covers TLS handshake

--- src/neo4j/_sync/io/_bolt_socket.py
+++ src/neo4j/_sync/io/_bolt_socket.py
@@ -327,7 +327,11 @@ class BoltSocket(BoltSocketBase):
             s = None
             try:
                 s = cls._connect_secure(
-                    resolved_address, tcp_timeout, keep_alive, ssl_context
+                    resolved_address,
+                    tcp_timeout,
+                    deadline,
+                    keep_alive,
+                    ssl_context,
                 )
                 agreed_version = s._handshake(resolved_address, deadline)
                 return s, agreed_version
