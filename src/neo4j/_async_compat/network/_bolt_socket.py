75c5c649f1fad81ed910052807b3754b297be3cb
Robsdedude <rouven.bauer@neo4j.com>
2026-06-02 10:32:13 +0200

    Fix async driver waiting for OS on closing half-open socket.
    This change aligns the async driver better with the sync driver.
    When a socket is closed, we don't want to wait until the OS acknowledges that
    the channel has been fully torn down. It's sufficient to flush all our data then
    tell the OS to close both ends of the channel. The rest we'll leave to the OS's
    TCP stack (and potentially other intermediates such as TLS wrappers) to figure
    out.
    
    Further, should the flushing respect any write timeouts.

--- src/neo4j/_async_compat/network/_bolt_socket.py
+++ src/neo4j/_async_compat/network/_bolt_socket.py
@@ -228,7 +228,7 @@ class AsyncBoltSocketBase(abc.ABC):
 
     async def close(self):
         self._writer.close()
-        await self._writer.wait_closed()
+        await self._wait_for_write(self._writer.drain)
 
     def kill(self):
         self._writer.close()
