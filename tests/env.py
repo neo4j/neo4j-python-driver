f23305e4f09311697695e20fae34d776d9317be5
Robsdedude <rouven.bauer@neo4j.com>
2026-06-29 14:18:34 +0200

    fixup! Python 3.14 + win: fix socket resource exhaustion

--- tests/env.py
+++ tests/env.py
@@ -98,7 +98,7 @@ NEO4J_SERVER_URI = _LazyEvalFunc(
     )
 )
 IS_WIN = sys.platform in {"win32", "cygwin"}
-IS_GHA = _LazyEvalEnv("GITHUB_ACTIONS", bool, default=False)
+IS_GHA = _LazyEvalEnv("GITHUB_ACTIONS", bool, default="false")
 
 
 __all__ = (
